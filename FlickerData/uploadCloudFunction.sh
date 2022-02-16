461de6cde774deb8bafb02f5a5b7ef154ddf870c
Hejiangtao-ecut <1471867575@qq.com>
2022-02-16 15:04:32 +0800

    微信小程序上手 demo

---
+++ FlickerData/uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e hjtwxapp-4g2fkowbb9335eab --n quickstartFunctions --r --project "/Users/hejiangtao02/Desktop/毕设项目/GraduationProject/FlickerData" --report_first --report

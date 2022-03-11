ab004c182c0b457cde1444d3daa2f728ec8fec2a
Hejiangtao-ecut <1471867575@qq.com>
2022-03-11 17:14:16 +0800

    云开发

--- FlickerData/src/pages/demo/index.js
+++ FlickerData/src/pages/demo/index.js
@@ -3,43 +3,45 @@
  * @desc 云开发 demo 页面
  */
 
+// 初始化数据库  wx.cloud.database()
+// 获取数据表  collection
+
+const DB = wx.cloud.database().collection("produce");
+
 Page({
 
     /**
     * 页面的初始数据
     */
     data: {
-
+        name: '',
+        age: 0,
+        delName: ''
     },
 
     /**
-    * 生命周期函数--监听页面加载
-    */
-    onLoad: function (options) {
-
+     * 获取输入名字的信息
+     */
+    getName(event) {
+        this.setData({
+            name:event.detail.value
+        });
+        console.log(this.data.name);
     },
 
     /**
-    * 生命周期函数--监听页面初次渲染完成
-    */
-    onReady: function () {
-
+     * 获取输入年龄的信息
+     */
+    getAge(event) {
+        this.setData({
+            age:event.detail.value
+        });
+        console.log(this.data.age);
     },
 
     /**
-    * 生命周期函数--监听页面显示
-    */
-    onShow: function () {
-
-    },
-
-    /**
-    * 生命周期函数--监听页面隐藏
-    */
-    onHide: function () {
-
-    },
-
+     * 调用云函数
+     */
     buttonTap() {
         wx.cloud.callFunction({
             name: "test",
@@ -57,30 +59,88 @@ Page({
     },
 
     /**
-    * 生命周期函数--监听页面卸载
-    */
-    onUnload: function () {
-
+     * 向数据库添加数据
+     */
+    addData() {
+        const { name, age } = this.data;
+        DB.add({
+            data: {
+                name,
+                age
+            },
+            success(res) {
+                console.log('添加成功');
+                console.log(res);
+            },
+            fail(res) {
+                console.log('添加失败');
+                console.log(res);
+            }
+        })
     },
 
     /**
-    * 页面相关事件处理函数--监听用户下拉动作
-    */
-    onPullDownRefresh: function () {
-
+     * 查询数据
+     */
+    getData() {
+        DB.get({
+            success(res) {
+                console.log('查询成功');
+                console.log(res);
+            },
+            fail(res) {
+                console.log('查询失败');
+                console.log(res);
+            }
+        })
     },
 
     /**
-    * 页面上拉触底事件的处理函数
-    */
-    onReachBottom: function () {
-
+     * 获取要删除的名字
+     */
+    getDelName(event) {
+        this.setData({
+            delName:event.detail.value
+        });
+        console.log(this.data.delName);
     },
 
     /**
-    * 用户点击右上角分享
-    */
-    onShareAppMessage: function () {
+     * 获取要删除的 id
+     */
+    async getDelId() {
+        let delId = '';
+        await DB
+            .where({
+                name: this.data.delName
+            })
+            .get()
+            .then(res => {
+                console.log(res);
+                delId = res.data[0] && res.data[0]._id;
+            });
+        return delId;
+    },
 
+    /**
+     * 删除数据
+     * 删除数据最好是获取到唯一值，不然会误删多条数据
+     * 所以删除数据前最好使用获需要删除的 id
+     */
+    async delData() {
+        const delId = await this.getDelId();
+        console.log('----');
+        if (delId) {
+            DB.doc(delId).remove({
+                success(res) {
+                    console.log('删除成功');
+                    console.log(res);
+                },
+                fail(res) {
+                    console.log('删除失败');
+                    console.log(res);
+                }
+            });
+        }
     }
 })
